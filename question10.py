d252d5ced84ce0a8811716dc9f211b9be2417900
ManavPatni <87895188+ManavPatni@users.noreply.github.com>
2021-09-10 12:02:47 +0530

    Add files via upload

---
+++ question10.py
@@ -0,0 +1,8 @@
+#Created by Manav Patni
+#Program to find and replace a given word
+
+string = input("Enter the full string: ")
+substring = input("Enter the substring to be replaced:")
+replacing_string = input("Enter the string to be replaced:")
+print (string.replace(substring, replacing_string))
+
